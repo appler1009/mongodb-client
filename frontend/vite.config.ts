dd9f20cfe38187729fdeae002a68d83300a771d4
Cheolgoo Kang <appler@gmail.com>
2025-06-22 23:05:26 -0700

    Fixing root directory problem

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: "0.0.0.0", // This makes the Vite dev server accessible from network interfaces
     port: 5173, // Your frontend port. Ensure this matches your docker-compose.yml
